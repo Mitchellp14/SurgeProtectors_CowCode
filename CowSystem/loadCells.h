f8de724224136c7676f2c451260a214d78eb63b7
Dakota-Augenstein <113377909+Dakota-Augenstein@users.noreply.github.com>
2026-03-12 14:31:47 -0600

    Update loadCells.h
    just starting to flesh out some direction

--- CowSystem/loadCells.h
+++ CowSystem/loadCells.h
@@ -1,9 +1,27 @@
-void setup() {
-  // put your setup code here, to run once:
+// loadCells.h
+// ADC / Load cell interface
+// configures ADC channel, reads that channel 
+// Compiles a overall sum of readings for output from either scale
 
-}
+#pragma once
+#include <Arduino.h>
 
-void loop() {
-  // put your main code here, to run repeatedly:
+class LoadCell_Tasks {
+public:
+  //enables and defines spi communication (may need to input spi pointer/s instead)
+  void begin_ADC(int arr_cs[], int Dout1, int Din1, int Dout2, int Din2); 
 
-}
+  //configures adc1 grabs measurment reconfigures grabs second measurment 
+  //configures adc2 grabs measurment reconfigures grabs second measurment
+  //Sums outputs and converts to weight
+  float ADC_Sum_Reading(int cs1, int cs2, (spi pointer or din / dout), int arr_cv[]);
+
+private:
+
+  //configures adc 
+void Config_ADC(...);
+
+
+
+
+};
